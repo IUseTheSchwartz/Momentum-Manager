534f31359332a5fab80eb933f5cb6fee442dbcbf
IUseTheSchwartz <83052876+IUseTheSchwartz@users.noreply.github.com>
2025-11-21 13:28:41 -0600

    Update Nav.jsx

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -94,6 +94,8 @@ export default function Nav() {
                 <Link to="/leads">Leads</Link>
                 <Link to="/trades">Trade Center</Link>
                 <Link to="/quote-tool">Quote Tool</Link>
+                {/* NEW: in-app Video Hub */}
+                <Link to="/videos">Videos</Link>
               </>
             )}
 
